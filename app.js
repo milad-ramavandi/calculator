10e950634f574ae3fa59fb0ba596f30a4b0ada32
milad ramavandi <miladramavandi@milads-MacBook-Air.local>
2023-08-29 19:03:14 +0330

    third commit

--- app.js
+++ app.js
@@ -2,37 +2,54 @@
 const screenCalculatorElement = document.querySelector(".screenCalculator");
 let powFlag = false;
 function addToScreenCalculator(value) {
-    screenCalculatorElement.value += value;
+    if (screenCalculatorElement.innerHTML == "0") {
+        screenCalculatorElement.innerHTML = value;
+    }
+    else {
+        screenCalculatorElement.innerHTML += value;
+    }
 }
 function calculate() {
     if (powFlag) {
-        const numbers = screenCalculatorElement.value.split("^");
+        const numbers = screenCalculatorElement.innerHTML.split("^");
         const result = Math.pow(Number(numbers[0]), Number(numbers[1]));
-        screenCalculatorElement.value = result.toString();
+        screenCalculatorElement.innerHTML = result.toString();
         powFlag = false;
     }
-    screenCalculatorElement.value = eval(screenCalculatorElement.value);
+    screenCalculatorElement.innerHTML = eval(screenCalculatorElement.innerHTML);
 }
 function clearScreenCalculator() {
-    screenCalculatorElement.value = "";
+    screenCalculatorElement.innerHTML = "0";
 }
 function squrtCalculate() {
-    const result = Math.sqrt(Number(screenCalculatorElement.value));
-    screenCalculatorElement.value = String(result);
+    const result = Math.sqrt(Number(screenCalculatorElement.innerHTML));
+    screenCalculatorElement.innerHTML = String(result);
 }
 function absoluteCalculate() {
-    const result = Math.abs(Number(screenCalculatorElement.value));
-    screenCalculatorElement.value = String(result);
+    const result = Math.abs(Number(screenCalculatorElement.innerHTML));
+    screenCalculatorElement.innerHTML = String(result);
 }
 function powerCalculate() {
-    screenCalculatorElement.value += "^";
+    screenCalculatorElement.innerHTML += "^";
     powFlag = true;
 }
 function sinCalculate() {
-    const radian = (Number(screenCalculatorElement.value) * Math.PI) / 180;
-    screenCalculatorElement.value = Math.sin(radian).toString();
+    const radian = (Number(screenCalculatorElement.innerHTML) * Math.PI) / 180;
+    screenCalculatorElement.innerHTML = Math.sin(radian).toString();
 }
 function cosCalculate() {
-    const radian = (Number(screenCalculatorElement.value) * Math.PI) / 180;
-    screenCalculatorElement.value = Math.cos(radian).toString();
+    const radian = (Number(screenCalculatorElement.innerHTML) * Math.PI) / 180;
+    screenCalculatorElement.innerHTML = Math.cos(radian).toString();
+}
+function factorialCalculate() {
+    let result = 1;
+    if (screenCalculatorElement.innerHTML === '0' || screenCalculatorElement.innerHTML === '1') {
+        screenCalculatorElement.innerHTML = result.toString();
+    }
+    else {
+        for (let index = Number(screenCalculatorElement.innerHTML); index >= 1; index--) {
+            result *= index;
+        }
+        screenCalculatorElement.innerHTML = result.toString();
+    }
 }
